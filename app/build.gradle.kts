15048930b66257e8204ab13b42686c9be7a3a469
mokurin000 <mokurin000@gmail.com>
2026-05-20 11:12:51 +0800

    build: fix storeType pcks12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,9 @@ android {
             enableV3Signing = true
 
             storeFile = file("../../keystore.jks")
+	    storeType = "pkcs12"
             storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+
             keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
             keyAlias = System.getenv("ANDROID_KEY_ALIAS")
         }
